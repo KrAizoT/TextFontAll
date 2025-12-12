f53b738b60a1964a79ffe5081dfcb06c1f56755b
Eol142 <rhegcrbq@gmail.com>
2025-12-12 11:51:44 +0100

    Auto Update Fonts

---
+++ Auto_Update.ps1
@@ -0,0 +1,13 @@
+# Name: KrAizoT
+# Create: 12/12/25
+# Modify: 12/12/25
+# GitHub_rep_name: https://github.com/KrAizoT/TextFontAll  # TextFontAll
+# Com: This Auto Update Git File 
+
+# --- CODE START --- 
+
+git add .
+git commit -m "Auto Update Fonts"
+git push
+
+# --- CODE END ---
